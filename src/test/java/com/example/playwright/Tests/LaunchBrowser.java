79b325c52a397bf8306259b1d6eb9f39b00ff554
Sergio Castillo Gutierrez <daw04.2022@gmail.com>
2024-03-04 16:42:10 +0100

    added slowMo and Pause

--- src/test/java/com/example/playwright/Tests/LaunchBrowser.java
+++ src/test/java/com/example/playwright/Tests/LaunchBrowser.java
@@ -11,9 +11,12 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 public class LaunchBrowser {
 
     @Test
-    public void testLogin(){
+    public void testLogin() throws InterruptedException {
         Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch(new LaunchOptions().setHeadless(true)); //Cuando esta a false, podemos ver el interfaz grafico
+        Browser browser = playwright.chromium().launch(
+                new LaunchOptions().setHeadless(false)
+                        .setSlowMo(1000)
+            ); //Cuando esta a false, podemos ver el interfaz grafico
         Page page = browser.newPage();
         page.navigate("https://ecommerce-playground.lambdatest.io/");
         Locator myAccount = page.locator("//a[contains(., 'My account')][@role='button']");
@@ -24,6 +27,7 @@ public class LaunchBrowser {
         page.getByPlaceholder("Password").type("1234");
         page.locator("//input[@value='Login']").click();
         assertThat(page).hasTitle("My Account");
+        Thread.sleep(3000);//*TODO :mirar el tiempo de pausa
         page.close();
         browser.close();
         playwright.close();
