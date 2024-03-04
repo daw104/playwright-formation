5d3f35baa47790279d3d41e1672104d4cb37bd40
Sergio Castillo Gutierrez <daw04.2022@gmail.com>
2024-03-04 12:08:16 +0100

    first commit

---
+++ src/main/java/com/example/playwright/PlaywRightApplication.java
@@ -0,0 +1,13 @@
+package com.example.playwright;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaywRightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaywRightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/playwright/Tests/LaunchBrowser.java
@@ -0,0 +1,33 @@
+package com.example.playwright.Tests;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType.LaunchOptions;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.Test;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+public class LaunchBrowser {
+
+    @Test
+    public void testLogin(){
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new LaunchOptions().setHeadless(true)); //Cuando esta a false, podemos ver el interfaz grafico
+        Page page = browser.newPage();
+        page.navigate("https://ecommerce-playground.lambdatest.io/");
+        Locator myAccount = page.locator("//a[contains(., 'My account')][@role='button']");
+        myAccount.hover();
+        page.locator("//a[contains(., 'Login')]").click();
+        assertThat(page).hasTitle("Account Login");
+        page.getByPlaceholder("E-Mail Address").type("i@i.com");
+        page.getByPlaceholder("Password").type("1234");
+        page.locator("//input[@value='Login']").click();
+        assertThat(page).hasTitle("My Account");
+        page.close();
+        browser.close();
+        playwright.close();
+    }
+
+
+}
